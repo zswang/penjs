2ca1af93a286d9ec9356d75582bf5f4c4d331773
zswang <wangjihu@sensorsdata.cn>
2017-04-25 17:59:41 +0800

    🎉 create

---
+++ gulpfile.js
@@ -0,0 +1,71 @@
+/*jshint globalstrict: true*/
+/*global require*/
+
+'use strict'
+
+const gulp = require('gulp')
+const connect = require('gulp-connect')
+const jdists = require('gulp-jdists')
+const uglify = require('gulp-uglify')
+const rename = require("gulp-rename")
+const replace = require('gulp-replace')
+const open = require('gulp-open')
+const examplejs = require('gulp-examplejs')
+
+const port = 20176;
+
+gulp.task('example', function() {
+  return gulp.src([
+      'src/*.js'
+    ])
+    .pipe(examplejs({
+      header: `
+global.jnodes = require('../src/jnodes.js');
+global.ejs = require('ejs');
+global.jhtmls = require('jhtmls');
+      `
+    }))
+    .pipe(gulp.dest('test'))
+})
+
+gulp.task('open', function() {
+  gulp
+    .src(__filename)
+    .pipe(open({
+      uri: `http://localhost:${port}/example/index.html`
+    }))
+})
+
+gulp.task('connect', function() {
+  connect.server({
+    port: port,
+    livereload: {
+      prot: 35731
+    }
+  })
+})
+
+gulp.task('watch', function() {
+  gulp.watch(['./example/*.html', './src/**/*.js'], ['build', 'reload'])
+})
+
+gulp.task('reload', function() {
+  gulp
+    .src(['./example/*.html', './src/**/*.js'], ['build', 'reload'])
+    .pipe(connect.reload())
+})
+
+gulp.task('build', function() {
+  gulp.src('./src/h5page.js')
+    .pipe(jdists({
+      trigger: 'release'
+    }))
+    .pipe(gulp.dest('./'))
+    .pipe(uglify())
+    .pipe(rename('h5page.min.js'))
+    .pipe(gulp.dest('./'))
+})
+
+gulp.task('default', ['build', 'connect', 'watch', 'open'])
+
+gulp.task('dist', ['build'])
---
+++ src/h5page.js
@@ -0,0 +1,156 @@
+(function (exportName) {
+
+  /*<jdists encoding="fndep" import="../node_modules/jnodes/jnodes.js" depend="Parser,Binder">*/
+  var jnodes = require('../node_modules/jnodes/jnodes.js');
+  var Binder = jnodes.Binder;
+  var Parser = jnodes.Parser;
+  /*</jdists>*/
+
+  /*<jdists encoding="fndep" import="../node_modules/jnodes/src/js/Compiler/jhtmls.js" depend="compiler_jhtmls">*/
+  var compiler_jhtmls = require('../node_modules/jnodes/src/js/Compiler/jhtmls.js');
+  /*</jdists>*/
+
+  /*<jdists encoding="fndep" import="../node_modules/jhtmls/jhtmls.js" depend="jhtmls_render">*/
+  var jhtmls_render = require('../node_modules/jhtmls/jhtmls.js').render;
+  /*</jdists>*/
+
+  /*<jdists encoding="fndep" import="../node_modules/h5tap/h5tap.js" depend="h5tap">*/
+  var h5tap = require('../node_modules/h5tap/h5tap.js');
+  /*</jdists>*/
+
+  var h5page_guid = 0;
+
+  var exports = function (element, options) {
+    options = options || {};
+    options.data = options.data || {};
+    if (typeof element === 'string') {
+      element = document.querySelector(element);
+    }
+    if (!element) {
+      return;
+    }
+    var parentElement;
+    var scriptElement;
+
+    if (/script/i.test(element.tagName)) {
+      scriptElement = element;
+      parentElement = element.parentNode;
+    } else {
+      scriptElement = element.querySelector('script');
+      parentElement = element;
+    }
+    if (!scriptElement || !parentElement) {
+      return;
+    }
+
+    var match = scriptElement.getAttribute('type').match(/text\/([\w]+)/);
+    if (!match) {
+      return;
+    }
+
+    function findEventTarget(parent, target, selector) {
+      var elements = [].slice.call(parent.querySelectorAll(selector));
+      while (target && elements.indexOf(target) < 0) {
+        target = target.parentNode;
+      }
+      return target;
+    }
+
+    /**
+     * 绑定方法的执行对象
+     *
+     * @param {Object} methods
+     * @param {Object} data
+     */
+    function methodsBind(methods, data) {
+      if (!methods) {
+        return;
+      }
+      var result = {};
+      Object.keys(methods).forEach(function (key) {
+        var value = methods[key];
+        if (typeof value === 'function') {
+          result[key] = function () {
+            value.apply(data, arguments);
+          };
+        } else {
+          result[key] = value;
+        }
+      });
+      return result;
+    }
+    var binderName = 'binder' + (h5page_guid++).toString(36);
+    var binder = h5page[binderName] = new Binder({
+      bindObjectName: 'h5page.' + binderName,
+      imports: methodsBind(options.methods, options.data),
+    });
+
+    if (typeof options.init === 'function') {
+      options.init.call(options.data, binder);
+    }
+
+    ['click', 'dblclick', 'keydown', 'keyup', 'focusin', 'focusout'].forEach(function (eventName) {
+      parentElement.addEventListener(eventName, function (e) {
+        if (e.target.getAttribute(binder._eventAttributePrefix + 'input')) {
+          if (eventName === 'focusin') {
+            e.target.addEventListener('input', function (e) {
+              binder.triggerScopeEvent(e);
+            });
+          } else if (eventName === 'focusout') {
+            e.target.removeEventListener('input', function (e) {
+              binder.triggerScopeEvent(e);
+            });
+          }
+        }
+
+        var target = findEventTarget(parentElement, e.target, '[' + binder._eventAttributePrefix + eventName + ']');
+        if (!target) {
+          return;
+        }
+        binder.triggerScopeEvent(e, target);
+      });
+    });
+
+    h5tap(parentElement, '[' + binder._eventAttributePrefix + 'tap]', function (element, e) {
+      binder.triggerScopeEvent({ type: 'tap', target: element });
+    });
+
+    binder.registerCompiler('jhtmls', function (templateCode, bindObjectName) {
+      var code = Parser.build(Parser.parse(templateCode), bindObjectName, compiler_jhtmls);
+      return jhtmls_render(code);
+    });
+    var templateType = match[1];
+    var templateRender = binder.templateCompiler(templateType, scriptElement.innerHTML);
+
+    if (templateRender) {
+      parentElement.innerHTML = templateRender(options.data || {});
+    }
+    binder.$$scope.element = parentElement;
+
+  };
+
+  exports.Parser = Parser;
+
+  /*<jdists encoding="fndep" import="../node_modules/h5ajax/h5ajax.js" depend="h5ajax_post,h5ajax_get">*/
+  var h5ajax = require('../node_modules/h5ajax/h5ajax.js');
+  var h5ajax_post = h5ajax.post;
+  var h5ajax_get = h5ajax.get;
+  /*</jdists>*/
+  exports.Ajax = {
+    post: h5ajax_post,
+    get: h5ajax_get,
+  };
+
+  /* istanbul ignore next */
+  if (typeof define === 'function') {
+    if (define.amd || define.cmd) {
+      define(function () {
+        return exports;
+      });
+    }
+  } else if (typeof module !== 'undefined' && module.exports) {
+    module.exports = exports;
+  } else {
+    window[exportName] = exports;
+  }
+})('h5page');
